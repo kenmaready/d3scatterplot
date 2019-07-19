e227e9da70d69daa8c01cf186f78521dc9071042
kenmaready <ken@maready.com>
2019-07-19 17:56:56 -0400

    Updates

---
+++ precache-manifest.b8036b8eb4841aef4b345f355511d4e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83acc35f098621ae6d3cb1c94bc91cf1",
+    "url": "/d3-scatterplot/index.html"
+  },
+  {
+    "revision": "7461be3e2808e2681c30",
+    "url": "/d3-scatterplot/static/css/main.ab98f870.chunk.css"
+  },
+  {
+    "revision": "f6c696417591743dc124",
+    "url": "/d3-scatterplot/static/js/2.2f16eede.chunk.js"
+  },
+  {
+    "revision": "7461be3e2808e2681c30",
+    "url": "/d3-scatterplot/static/js/main.e9007b41.chunk.js"
+  },
+  {
+    "revision": "38338e7d8507517174b3",
+    "url": "/d3-scatterplot/static/js/runtime~main.73fe1af9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/d3scatterplot/precache-manifest.334ae8cf2f9121b1622c8980f0343f20.js"
+  "/d3-scatterplot/precache-manifest.b8036b8eb4841aef4b345f355511d4e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/d3scatterplot/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/d3-scatterplot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
