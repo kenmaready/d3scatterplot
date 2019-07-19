2d0c21829453107d7457b9e4b9cafde33b7a7b53
kenmaready <ken@maready.com>
2019-07-19 17:35:49 -0400

    Updates

---
+++ precache-manifest.334ae8cf2f9121b1622c8980f0343f20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d410063674a396f5e3e759bbc64875a",
+    "url": "/d3scatterplot/index.html"
+  },
+  {
+    "revision": "7461be3e2808e2681c30",
+    "url": "/d3scatterplot/static/css/main.ab98f870.chunk.css"
+  },
+  {
+    "revision": "f6c696417591743dc124",
+    "url": "/d3scatterplot/static/js/2.2f16eede.chunk.js"
+  },
+  {
+    "revision": "7461be3e2808e2681c30",
+    "url": "/d3scatterplot/static/js/main.e9007b41.chunk.js"
+  },
+  {
+    "revision": "6ac856323a66b47cec1e",
+    "url": "/d3scatterplot/static/js/runtime~main.bcc13cd2.js"
+  }
+]);
